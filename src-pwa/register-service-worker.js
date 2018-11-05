fe5a27bb3aad7685df8be55cbb9b0f10e4aa033e
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2018-11-04 22:00:36 -0200

    Retirando configs de PWA

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -7,26 +7,21 @@ import { register } from 'register-service-worker'
 
 register(process.env.SERVICE_WORKER_FILE, {
   ready () {
-    alert('App is being served from cache by a service worker.')
     console.log('App is being served from cache by a service worker.')
   },
   registered (registration) { // registration -> a ServiceWorkerRegistration instance
     console.log('Service worker has been registered.')
   },
   cached (registration) { // registration -> a ServiceWorkerRegistration instance
-    alert('Content has been cached for offline use.')
     console.log('Content has been cached for offline use.')
   },
   updatefound (registration) { // registration -> a ServiceWorkerRegistration instance
-    alert('New content is downloading.')
     console.log('New content is downloading.')
   },
   updated (registration) { // registration -> a ServiceWorkerRegistration instance
-    alert('New content is available; please refresh.')
     console.log('New content is available; please refresh.')
   },
   offline () {
-    alert('No internet connection found. App is running in offline mode.')
     console.log('No internet connection found. App is running in offline mode.')
   },
   error (err) {
